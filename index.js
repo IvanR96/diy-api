46d328af57e15d992958aaf575b886ee7c9e9b33
IvanR96 <irios96@hotmail.com>
2024-09-14 23:53:03 -0700

    new specific joke request added

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ app.get("/random", (req, res)=>{
 });
 
 //2. GET a specific joke
-app.get("/jokes/id", (req, res)=>{
-
+app.get("/jokes/:id", (req, res)=>{
+  const id = parseInt(req.params.id);
+  const foundJoke = jokes.find(joke)
 }) ;
 
 //3. GET a jokes by filtering on the joke type
