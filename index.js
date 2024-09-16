1d837c4180323dd294310bcd0a83513f84b943df
IvanR96 <irios96@hotmail.com>
2024-09-15 23:56:44 -0700

    new request added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser";
+import bodyParser, { json } from "body-parser";
 
 const app = express();
 const port = 3000;
@@ -17,10 +17,16 @@ app.get("/random", (req, res)=>{
 //2. GET a specific joke
 app.get("/jokes/:id", (req, res)=>{
   const id = parseInt(req.params.id);
-  const foundJoke = jokes.find(joke)
-}) ;
+  const foundJoke = jokes.find((joke)=> joke.id === id);
+  res.json(foundJoke);
+});
 
 //3. GET a jokes by filtering on the joke type
+app.get("/filter", (req,res)=>{
+  const type = req.query.type;
+  const filteredActivities = jokes.filter((joke)=> joke.jokeType === type);
+  res.json(filteredActivities);
+});
 
 //4. POST a new joke
 
